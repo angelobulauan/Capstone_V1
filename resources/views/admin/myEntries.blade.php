2d77927bcc64fc6ea588e5313452b47cdb3ac73f
ginxFromYt <55861477+ginxFromYt@users.noreply.github.com>
2024-05-12 14:15:43 +0800

    Bug Fix on update photos

--- resources/views/admin/myEntries.blade.php
+++ resources/views/admin/myEntries.blade.php
@@ -15,7 +15,7 @@
                         <img src="{{ asset('storage/' . $d->photo) }}" alt="First Photo" class="img-fluid"
                             id="imageToSelect-{{ $d->id }}">
                         <button type="button" class="btn btn-primary mt-2" data-bs-toggle="modal"
-                            data-bs-target="#imageSelectionModal">
+                            data-bs-target="#imageSelectionModal-{{ $d->id }}">
                             Update Image
                         </button>
                     </div>
@@ -25,19 +25,19 @@
 
                         <div class="card">
                             <div class="card-body">
-
                                 <div class="row">
-                                    <div class="col-sm-6 p-1"> Name: {{ $d->name }}</div>
+                                    <div class="col-sm-6 p-1">Name: {{ $d->name }}</div>
                                     <div class="col-sm-6 p-1">Scientific Name: {{ $d->scientificname }}</div>
-                                    <div class="col-sm-6 p-1"><i
-                                            class="fa fa-info-circle mr-2 text-primary"></i>Description:
-                                        {{ $d->description }}</div>
-                                    <div class="col-sm-6 p-1"><i class="fa fa-map-marker mr-2 text-warning"></i>Location:
-                                        {{ $d->location }}</div>
-                                    <div class="col-sm-6 p-1"><i class="fa fa-map-pin mr-2"></i>Abundance:
-                                        {{ $d->abundance }}</div>
-
-
+                                    <div class="col-sm-6 p-1">
+                                        <i class="fa fa-info-circle mr-2 text-primary"></i>Description:
+                                        {{ $d->description }}
+                                    </div>
+                                    <div class="col-sm-6 p-1">
+                                        <i class="fa fa-map-marker mr-2 text-warning"></i>Location: {{ $d->location }}
+                                    </div>
+                                    <div class="col-sm-6 p-1">
+                                        <i class="fa fa-map-pin mr-2"></i>Abundance: {{ $d->abundance }}
+                                    </div>
 
                                     <div class="col-sm-6 p-1">Created At: {{ $d->created_at }}</div>
                                     <div class="row mt-0">
@@ -48,17 +48,16 @@
                                             </a>
 
                                             <div class="p-1">
-                                                <form action="{{ route('delete.ko', ['d_id' => $d->id]) }}" method="post" class="selec">
-                                                    @csrf
-                                                    @method('DELETE')
+                                                <form action="{{ route('delete.ko', ['d_id' => $d->id]) }}" method="post"
+                                                    class="selec">
+                                                    @csrf @method('DELETE')
 
-                                                    <button type="submit"class="btn btn-danger py-0">Remove</button>
+                                                    <button type="submit" class="btn btn-danger py-0">Remove</button>
                                                 </form>
                                             </div>
                                         </div>
-                                        <!-- </div> -->
-                                    </div>
 
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -67,37 +66,42 @@
                 </div>
                 <hr>
             </div>
-    </div>  {{-- end of conatiner --}}
 
+            <!-- Modal for Image Selection -->
+            <div class="modal fade" id="imageSelectionModal-{{ $d->id }}" tabindex="-1"
+                aria-labelledby="imageSelectionModalLabel" data-bs-backdrop="static" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="imageSelectionModalLabel">Select an Image</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body" id="imageOptions">
+                            <!-- Iterate through each photo fetched from the database -->
+                            @foreach ($selectphoto as $photo)
+                                <form action="{{ route('admin.update-photo', ['id' => $d->id, 'photo' => $photo->id]) }}"
+                                    class="update-photo-form" method="POST"
+                                    style="display: inline-block; margin-right: 10px;">
+                                    <!-- CSRF token for form security -->
+                                    @csrf
+                                    @php
+                                        // dd($photo);
+                                    @endphp
 
-    <div class="modal fade" id="imageSelectionModal" tabindex="-1" aria-labelledby="imageSelectionModalLabel"
-        data-bs-backdrop="static" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="imageSelectionModalLabel">Select an Image</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    <!-- Display the image using the path stored in the database -->
+                                    <button type="submit" style="background: none; border: none; padding: 0;">
+                                        <img src="{{ asset('storage/' . $photo->Photo) }}" alt="Photo"
+                                            style="width: 125px; height: 125px; display: inline-block;">
+                                    </button>
+                                </form>
+                            @endforeach
+                        </div>
+
+                    </div>
                 </div>
-                <div class="modal-body" id="imageOptions">
-                    <!-- Iterate through each photo fetched from the database -->
-                    @foreach ($selectphoto as $photo)
-                        <form action="{{ route('admin.update-photo', ['id' => $d->id, 'photo' => $photo->id])}}" class="update-photo-form"
-                            method="POST" style="display: inline-block; margin-right: 10px;">
-                            <!-- CSRF token for form security -->
-                            @csrf
-
-                            <!-- Display the image using the path stored in the database -->
-                            <button type="submit" style="background: none; border: none; padding: 0;">
-                                <img src="{{ asset('storage/' . $photo->Photo) }}" alt="Photo"
-                                    style="width: 125px; height: 125px; display: inline-block;">
-                            </button>
-                        </form>
-                    @endforeach
-                </div>
-
             </div>
-        </div>
-    </div>
+        @endforeach
+    </div> {{-- end of container --}}
 
     <!-- Script -->
     <script>
@@ -144,5 +148,4 @@
             });
         });
     </script>
-    @endforeach
 @endsection
