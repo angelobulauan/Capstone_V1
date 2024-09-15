06ff72411fa1530f70c4c4b8a72ae2b554fb1074
angelobulauan <Bulauan.angelo03@gmail.com>
2024-09-15 13:09:38 +0800

    updating navbar

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -2,8 +2,9 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 dark:border-indigo-600 text-sm font-medium leading-5 text-gray-900 dark:text-gray-100 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
-            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 hover:border-gray-300 dark:hover:border-gray-700 focus:outline-none focus:text-gray-700 dark:focus:text-gray-300 focus:border-gray-300 dark:focus:border-gray-700 transition duration-150 ease-in-out';
+    ? 'inline-flex items-center px-3 py-2 border-b-2 border-indigo-400 dark:border-indigo-600 text-lg font-semibold font-sans leading-6 text-gray-900 dark:text-gray-100 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out no-underline hover:text-black dark:hover:text-black hover:bg-blue-100 dark:hover:bg-blue-800 hover:shadow-lg transform hover:scale-105 active:text-black active:bg-transparent active:border-transparent active:scale-95'
+    : 'inline-flex items-center px-3 py-2 border-b-2 border-transparent text-lg font-semibold font-sans leading-6 text-gray-500 dark:text-gray-400 hover:text-black dark:hover:text-black hover:border-blue-500 dark:hover:border-blue-400 hover:bg-blue-100 dark:hover:bg-blue-800 hover:shadow-lg transform hover:scale-105 active:text-black active:bg-transparent active:border-transparent active:scale-95 transition duration-150 ease-in-out no-underline';
+
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
