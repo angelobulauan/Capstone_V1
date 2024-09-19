8bf96c457a7b6ee3b2591888ffba08ab35eff9b3
angelobulauan <Bulauan.angelo03@gmail.com>
2024-09-19 13:21:56 +0800

    updating some codes

--- resources/views/components/dropdown-link.blade.php
+++ resources/views/components/dropdown-link.blade.php
@@ -1,1 +1,3 @@
-<a {{ $attributes->merge(['class' => 'block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-800 transition duration-150 ease-in-out']) }}>{{ $slot }}</a>
+<a {{ $attributes->merge(['class' => 'block w-full px-4 py-2 text-start text-lg leading-5 text-gray-700 dark:text-gray-300 hover:bg-blue-500 dark:hover:bg-blue-500 hover:text-black focus:outline-none focus:bg-blue-500 dark:focus:bg-blue-500 transition duration-150 ease-in-out no-underline font-sans font-bold italic']) }}>
+    {{ $slot }}
+</a>
