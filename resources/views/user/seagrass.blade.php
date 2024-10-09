9348fcac70c8d76a0592dc9cc576a0a01907370c
Prog Harvey <deseoharvey5@gmail.com>
2024-10-09 15:08:49 +0800

    fixed like, dislike, view for viewing seagrasses

--- resources/views/user/seagrass.blade.php
+++ resources/views/user/seagrass.blade.php
@@ -3,15 +3,25 @@
 @section('content')
     @php
         $myEntry = DB::table('seaviews')
-            ->orderBy('created_at', 'desc') // Order by the 'created_at' column in descending order
+            ->orderBy('created_at', 'desc')
+            ->get();
+    @endphp
+
+    @php
+        $interactions = DB::table('sea_grass_likes')
+            ->select(
+                'seaviews_id',
+                DB::raw('SUM(likes) as total_likes'),
+                DB::raw('SUM(dislikes) as total_dislikes'),
+                DB::raw('SUM(views) as total_views'),
+            )
+            ->groupBy('seaviews_id')
             ->get();
     @endphp
 
     @foreach ($myEntry as $d)
         @php
-            $interaction = DB::table('sea_grass_likes')
-                ->where('sea_grass_likes.seaviews_id', $d->id)
-                ->first();
+            $interaction = $interactions->firstWhere('seaviews_id', $d->id);
         @endphp
         <div class="container">
             <div class="row mt-3">
@@ -38,21 +48,26 @@
 
                                 <div class="row mt-0">
                                     <div class="col-sm-3 d-flex justify-content-start align-items-center">
-                                        <button type="button" class="btn btn-primary viewMapBtn" data-id="{{ $d->id }}">
+                                        <button type="button" class="btn btn-primary viewMapBtn"
+                                            data-id="{{ $d->id }}">
                                             View Location
                                         </button>
                                     </div>
                                     <div class="col-sm-9 d-flex justify-content-end align-items-center">
                                         <a href="javascript:void(0);" class="p-4 likeBtn" data-id="{{ $d->id }}">
                                             <i class="fas fa fa-thumbs-up fa-2x text-black-300"></i>
-                                            <span id="like-count-{{ $d->id }}">{{ $interaction->likes ?? 0 }}</span>
+                                            <span
+                                                id="like-count-{{ $d->id }}">{{ $interaction->total_likes ?? 0 }}</span>
                                         </a>
                                         <a href="javascript:void(0);" class="dislikeBtn" data-id="{{ $d->id }}">
                                             <i class="fas fa fa-thumbs-down fa-2x text-red-300"></i>
                                             <span
-                                               id="dislike-count-{{ $d->id }}">{{ $interaction->dislikes ?? 0 }}</span>
+                                                id="dislike-count-{{ $d->id }}">{{ $interaction->total_dislikes ?? 0 }}</span>
                                         </a>
-                                        <p class="text-danger pl-4 mt-4"><span>{{ $interaction->views ?? 0 }}</span> <i class="fa fa-eye" aria-hidden="true"></i>
+                                        <p class="text-danger pl-4 mt-4">
+                                            <span
+                                                id="view-count-{{ $d->id }}">{{ $interaction->total_views ?? 0 }}</span>
+                                            <i class="fa fa-eye" aria-hidden="true"></i>
                                         </p>
                                     </div>
                                 </div>
@@ -64,7 +79,6 @@
             <hr>
         </div>
 
-        <!-- Each entry gets its own modal -->
         <div class="modal mapCont" id="mapModal-{{ $d->id }}" tabindex="-1" role="dialog" style="display:none;">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
@@ -76,7 +90,6 @@
                         </button>
                     </div>
                     <div class="modal-body bg-slate-200">
-                        <!-- The map will be embedded here -->
                         <div id="map-{{ $d->id }}" style="height: 400px;"></div>
                     </div>
                     <div class="modal-footer">
@@ -90,16 +103,18 @@
     <script src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_MAPS_API_KEY') }}"></script>
     <script>
         $(document).ready(function() {
-            // Show map and initialize it on button click
             $(document).on('click', '.viewMapBtn', function() {
                 var seaviewId = $(this).data('id');
 
                 $.ajax({
-                    url: '/user/updateView/' + seaviewId,
+                    url: '/user/updateView/' +
+                    seaviewId,
                     type: 'GET',
                     success: function(response) {
                         console.log('Response received:', response);
+
                         if (response && response.views !== undefined) {
+                            $('#view-count-' + seaviewId).text(response.views);
                             console.log('View count updated. Total views: ' + response.views);
                         } else {
                             console.log('Unexpected response format:', response);
@@ -114,36 +129,40 @@
                 initializeMap(seaviewId);
             });
 
-            // Hide the map container when the close button is clicked
             $(document).on('click', '.closeMapModalBtn', function() {
                 var id = $(this).data('id');
                 $("#mapModal-" + id).fadeOut();
             });
 
-            // Map initialization function
             function initializeMap(id) {
                 var map = new google.maps.Map(document.getElementById('map-' + id), {
-                    center: {lat: 0, lng: 0},
+                    center: {
+                        lat: 0,
+                        lng: 0
+                    },
                     zoom: 2
                 });
 
-                // Find the data for the current ID
                 @foreach ($myEntry as $d)
                     if ({{ $d->id }} == id) {
                         var marker = new google.maps.Marker({
-                            position: {lat: {{ $d->lati }}, lng: {{ $d->longti }}},
+                            position: {
+                                lat: {{ $d->lati }},
+                                lng: {{ $d->longti }}
+                            },
                             map: map,
                             title: "{{ $d->name }}"
                         });
 
-                        // Center map on the marker
-                        map.setCenter({lat: {{ $d->lati }}, lng: {{ $d->longti }}});
+                        map.setCenter({
+                            lat: {{ $d->lati }},
+                            lng: {{ $d->longti }}
+                        });
                         map.setZoom(13);
                     }
                 @endforeach
             }
 
-            // Handle like button click
             $(document).on('click', '.likeBtn', function(e) {
                 e.preventDefault();
                 var seaviewId = $(this).data('id');
@@ -152,9 +171,9 @@
                     url: '/user/like/' + seaviewId,
                     type: 'GET',
                     success: function(response) {
-                        // Update the likes count on the page
                         $('#like-count-' + seaviewId).text(response.likes);
-                        // alert(response.message); // Display the message
+                        $('#dislike-count-' + seaviewId).text(
+                            0);
                         Swal.fire({
                             icon: 'success',
                             title: 'Success',
@@ -162,12 +181,11 @@
                         });
                     },
                     error: function(xhr) {
-                        console.log(xhr.responseText); // Handle error
+                        console.log('Like AJAX error:', xhr.responseText);
                     }
                 });
             });
 
-            // Handle dislike button click
             $(document).on('click', '.dislikeBtn', function(e) {
                 e.preventDefault();
                 var seaviewId = $(this).data('id');
@@ -176,9 +194,9 @@
                     url: '/user/dislike/' + seaviewId,
                     type: 'GET',
                     success: function(response) {
-                        // Update the dislikes count on the page
                         $('#dislike-count-' + seaviewId).text(response.dislikes);
-                        // alert(response.message); // Display the message
+                        $('#like-count-' + seaviewId).text(
+                            0);
                         Swal.fire({
                             icon: 'success',
                             title: 'Success',
@@ -186,10 +204,11 @@
                         });
                     },
                     error: function(xhr) {
-                        console.log(xhr.responseText); // Handle error
+                        console.log('Dislike AJAX error:', xhr.responseText);
                     }
                 });
             });
+
         });
     </script>
 @endsection
