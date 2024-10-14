d78c130c58f5d48ebab4d65a1c2336830e8e6e0f
Prog Harvey <deseoharvey5@gmail.com>
2024-10-14 11:12:18 +0800

    commit

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,22 +2,21 @@
 @section('content')
     @php
         $seagrassCount = DB::table('seaviews')->count();
-
         $totalLikes = DB::table('sea_grass_likes')->where('likes', 1)->count();
-        $totaldisLikes = DB::table('sea_grass_likes')->where('dislikes', 1)->count();
+        $totalDislikes = DB::table('sea_grass_likes')->where('dislikes', 1)->count();
         $totalViews = DB::table('sea_grass_likes')->sum('views');
         $pendingApproval = DB::table('seaviews')->where('status', 'pending')->count();
 
-        /* pre tuloy muntu lang diffre=nt tables
-
-    tables ->likes, dislike, mostviewd, mostsearched,
-    */
-
+        $userActivity = DB::table('users')
+            ->selectRaw("DATE_FORMAT(created_at, '%M') as label, COUNT(*) as y")
+            ->groupByRaw("DATE_FORMAT(created_at, '%Y-%m'), DATE_FORMAT(created_at, '%M')")
+            ->orderByRaw('MIN(created_at)')
+            ->get()
+            ->toArray();
     @endphp
+
     <div class="container mt-5">
-        <div class="row ">
-
-
+        <div class="row">
             <div class="col-xl-3 col-md-6 mb-4x">
 
                 <div class="card text-center" style="transition: all 0.3s ease-in-out;">
@@ -109,7 +108,8 @@
                         <div class="card-header bg-info text-white">
                             <div class="row  align-items-center">
                                 <div class="col">
-                                    <i class="fas fa-hourglass-half fa-4x text-black-300 wobble-on-hover"></i> <!-- Grass icon -->
+                                    <i class="fas fa-hourglass-half fa-4x text-black-300 wobble-on-hover"></i>
+                                    <!-- Grass icon -->
 
                                 </div>
                                 <div class="col">
@@ -162,11 +162,12 @@
                     <div class="card-header bg-danger text-white">
                         <div class="row align-items-center">
                             <div class="col">
-                                <i class="fas fa fa-thumbs-down  fa-4x text-black-300 wobble-on-hover"></i> <!-- Grass icon -->
+                                <i class="fas fa fa-thumbs-down  fa-4x text-black-300 wobble-on-hover"></i>
+                                <!-- Grass icon -->
 
                             </div>
                             <div class="col">
-                                <h3 class="display-4">{{ $totaldisLikes }}</h3>
+                                <h3 class="display-4">{{ $totalDislikes }}</h3>
                                 <h6 class="text-uppercase mb-1">Dislikes</h6>
                             </div>
                         </div>
@@ -207,6 +208,78 @@
 
             </div>
         </div>
-    </div>
-@endsection
 
+        <!-- Chart section -->
+        <div class="row">
+            <div class="col-md-6 mt-5">
+                <div class="card">
+                    <div class="card-header bg-primary text-white">
+                        Likes vs Dislikes
+                    </div>
+                    <div class="card-body">
+                        <div id="likesDislikesChart" style="height: 300px; width: 100%;"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 mt-5">
+                <div class="card">
+                    <div class="card-header bg-secondary text-white">
+                        User Activity
+                    </div>
+                    <div class="card-body">
+                        <div id="userActivityChart" style="height: 300px; width: 100%;"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+    <script>
+        window.onload = function() {
+            // Likes vs Dislikes Pie Chart
+            var likesDislikesChart = new CanvasJS.Chart("likesDislikesChart", {
+                animationEnabled: true,
+                theme: "light",
+                title: {
+                    text: "Likes and Dislikes Distribution"
+                },
+                data: [{
+                    type: "doughnut",
+                    indexLabel: "{label}: {y}",
+                    yValueFormatString: "#,##0",
+                    dataPoints: [{
+                            label: "Likes",
+                            y: {{ $totalLikes }},
+                            color: "blue"
+                        },
+                        {
+                            label: "Dislikes",
+                            y: {{ $totalDislikes }},
+                            color: "red"
+                        }
+                    ]
+                }]
+            });
+            likesDislikesChart.render();
+
+            // User Activity Line Graph
+            var userActivityChart = new CanvasJS.Chart("userActivityChart", {
+                animationEnabled: true,
+                theme: "light2",
+                title: {
+                    text: "User Activity Over Time"
+                },
+                axisY: {
+                    title: "Number of Users"
+                },
+                data: [{
+                    type: "line",
+                    indexLabelFontSize: 16,
+                    dataPoints: @json($userActivity)
+                }]
+            });
+            userActivityChart.render();
+        }
+    </script>
+@endsection
